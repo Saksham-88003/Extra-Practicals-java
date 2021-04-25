b7730bfb295a5cd0a1c46971da026843b17ceb16
Saksham Dobriyal <81918552+Saksham-88003@users.noreply.github.com>
2021-04-26 00:37:58 +0530

    Add files via upload

---
+++ TwoDArray.java
@@ -0,0 +1,30 @@
+// 4) To learn use of two dimensional array by defining the array dynamically.
+class TwoDArray{
+    public static void main(String[] args){
+         int arr[][]= new int[5][];
+
+         arr[0]= new int[1];
+         arr[1]= new int[2];
+         arr[2]= new int[3];
+         arr[3]= new int[4];
+         arr[4]= new int[5];
+         int k=1;
+
+         for(int i=0; i<5; i++){
+             for(int j=0; j<=i; j++){
+                 arr[i][j]=k;
+                 k++;
+             }
+         }
+        System.out.println("The initialized array is : ");
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print(arr[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
