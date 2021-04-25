d977e99582c853929f97f54f12860d0edfe6fa0a
Saksham Dobriyal <81918552+Saksham-88003@users.noreply.github.com>
2021-04-25 16:15:02 +0530

    Add files via upload

---
+++ SumOfArguments.java
@@ -0,0 +1,13 @@
+
+// 1.) To find the sum of any integers entered as command line arguments.
+
+  class SumOfArguments{
+        public static void main(String [] args){
+            int n= args.length;
+            int sum=0;
+            for(int i=0 ; i<n ; i++){
+                sum+=Integer.parseInt(args[i]);
+            }
+            System.out.print("The sum of entered arguments is :"+sum);
+        }
+ }
